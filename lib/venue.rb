e39e6ae7a71beca99e7245d9fd8857d02740e63b
Mackenzie Leng <kenz.joy@outlook.com>
2022-08-24 10:47:53 -0600

    Add my ic tests

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,26 @@
 class Venue
+    attr_reader :name, 
+                :capacity,
+                :patrons
+
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @patrons = []
+    end
+
+    def add_patron(patron_name)
+        patrons << patron_name
+    end
+
+    def yell_at_patrons
+        patrons.map(&:upcase)
+    end
+
+    def over_capacity(capacity)
+        if patrons.length <= capactiy 
+            puts false
+        end
+    end
+
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -4,25 +4,25 @@ require './lib/venue'
 describe Venue do
   describe '#initialize' do
     it 'is a venue' do
-      skip
+      # skip
       venue = Venue.new('Bluebird', 4)
       expect(venue).to be_a Venue
     end
 
     it 'can read the name' do
-      skip
+      # skip
       venue = Venue.new('Bluebird', 4)
       expect(venue.name).to eq 'Bluebird'
     end
 
     it 'can read the capacity' do
-      skip
+      # skip
       venue = Venue.new('Bluebird', 4)
       expect(venue.capacity).to eq 4
     end
 
     it 'has no patrons by default' do
-      skip
+      # skip
       venue = Venue.new('Bluebird', 4)
       expect(venue.patrons).to eq []
     end
@@ -32,7 +32,7 @@ describe Venue do
 
   describe '#add_patron' do
     it 'returns a list of patrons' do
-      skip
+      # skip
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
@@ -44,12 +44,23 @@ describe Venue do
 
   describe '#yell_at_patrons' do
     it 'returns a list of uppercased names' do
-      skip
+      # skip
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
       venue.add_patron('Bob')
       expect(venue.yell_at_patrons).to eq ['MIKE', 'MEGAN', 'BOB']
     end
+
+    describe '#over_capacity' do
+      it 'return true or false if the venue is over capacity' do
+        expect(venue).to respond_to(over_capacity).with(1).argument
+        if (venue.patrons) <= (capacity)
+        expect(venue.over_capacity). to eq(false)
+        end
+      end
   end
 end
+end
+
+ 
